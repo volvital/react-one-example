24543cccbfa3bf0a41740761c2544ecdc006e85f
VolVital <98488542+volvital@users.noreply.github.com>
2023-02-11 20:26:08 +0200

    Updated AddTodo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
+import AddTodo from "./Todo/AddTodo";
 
 function App() {
   const [todos, setTodos] = React.useState([
@@ -24,10 +25,21 @@ function App() {
     setTodos(todos.filter(todo => todo.id !== id))
   }
 
+  function addTodo(title) {
+    setTodos(todos.concat([
+      {
+        title,
+        id: Date.now(),
+        completed: false
+      }
+  ]))
+  }
+
   return (
     <Context.Provider value={{ removeTodo }}>
       <div className="wrapper">
         <h1>Note book</h1>
+        <AddTodo onCreate={addTodo}/>
         {todos.length ? <TodoList todos={todos} onToggle={toggleTodo}/> : <p>No todos!!!</p>}
 
       </div>
